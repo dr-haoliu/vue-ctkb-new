d7e70eb7b8d36e430652145bdca8e3d83d8c94ab
liuhao <hl2659@columbia.edu>
2021-10-19 22:35:46 -0400

    Adding API page.

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue'
 import SearchCondition from "../views/SearchCondition";
 import SearchCriteria from "../views/SearchCriteria";
 import CriteriaList from "../views/CriteriaList";
+import API from "../views/API";
 
 Vue.use(VueRouter)
 
@@ -38,6 +39,14 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/CriteriaList')
+    },
+    {
+        path: '/API',
+        name: 'API',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/API')
     }
 ]
 
---
+++ src/views/API.vue
@@ -0,0 +1,73 @@
+<template>
+  <div style="width: 80%; flex: auto; margin: auto; padding: 10px">
+    <div style="font-size: 32px; padding: 10px">
+      <span><b>APIs</b> </span>
+    </div>
+
+    <el-container
+        v-loading="loading"
+        element-loading-text="Loading..."
+        element-loading-spinner="el-icon-loading"
+        element-loading-background="rgba(0, 0, 0, 0.8)"
+        style="width: 80%; margin: auto; padding: 10px; display: flex; flex-direction: column;">
+        <el-header>
+          <h4> $apiUrl = "http://34.69.146.90:8081"</h4>
+        </el-header>
+
+        <el-main>
+          <el-card>
+            <b>Search all criteria by a condition name:</b>
+            <div>
+              <p> $apiUrl + "/common-condition/get-criteria-name/{concept_name}/{include}"</p>
+              <p> replace concept_name with condition name you want to search</p>
+              <p> replace include with 1 for inclusion, 0 for exclusion</p>
+            </div>
+          </el-card>
+
+          <el-card>
+            <b>Search all criteria by a condition id:</b>
+            <div>
+              <p> $apiUrl + "/common-condition/get-criteria-id/{concept_id}/{include}"</p>
+              <p> replace concept_id with condition id you want to search</p>
+              <p> replace include with 1 for inclusion, 0 for exclusion</p>
+            </div>
+          </el-card>
+
+          <el-card>
+            <b>Search conditions by a criterion name:</b>
+            <div>
+              <p> $apiUrl + "/common-condition/get-conditions-name/{concept_name}/{include}"</p>
+              <p> replace concept_name with a criterion name you want to search</p>
+              <p> replace include with 1 for inclusion, 0 for exclusion</p>
+            </div>
+          </el-card>
+
+          <el-card>
+            <b>Search conditions by a criterion id:</b>
+            <div>
+              <p> $apiUrl + "/common-condition/get-conditions-id/{concept_id}/{include}"</p>
+              <p> replace concept_id with criterion id you want to search</p>
+              <p> replace include with 1 for inclusion, 0 for exclusion</p>
+            </div>
+          </el-card>
+
+        </el-main>
+
+
+      <el-row style="width: 80%; margin: auto; margin-top: 10px; display: flex;">
+        <div><b>Please contact us for more detailed API information.</b></div>
+      </el-row>
+
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "API"
+}
+</script>
+
+<style scoped>
+
+</style>
